7a726c8352823270e79057c82554b6e795a0ced5
edavis47 <59838021+edavis47@users.noreply.github.com>
2020-01-27 07:55:24 -0500

    Add files via upload

---
+++ CalculatingAverages/ADD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        //Add 10 numbers
+        Console.WriteLine("Enter 10 numbers between 0-100:");
+        int num0 = int.Parse(Console.ReadLine());
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+        int num4 = int.Parse(Console.ReadLine());
+        int num5 = int.Parse(Console.ReadLine());
+        int num6 = int.Parse(Console.ReadLine());
+        int num7 = int.Parse(Console.ReadLine());
+        int num8 = int.Parse(Console.ReadLine());
+        int num9 = int.Parse(Console.ReadLine());
+
+        int sum = num0 + num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9;
+        Console.WriteLine($"Sum: {sum.ToString()}");
+    }
+}
---
+++ CalculatingAverages/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingAverages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+        void run()
+        {
+            //BASIC DIFFICULTY
+            //SUM OF 10 SCORES:
+
+            //Console.WriteLine("To calculate the sum of 10 scores:");
+            //Console.WriteLine($"The sum is {CalculateSum()}");
+
+
+            //AVERAGE 10 SCORES
+
+            //Console.WriteLine("To calculate the average of 10 scores:");
+            //double average = CalculateSum() / 10;
+            //Console.WriteLine($"The average is {average}; the letter grade is {CalculateGrade(average)}");
+
+
+
+
+            //INTERMEDIATE DIFFICULTY
+            //AVERAGE A SPECIFIC NUMBER OF SCORES:
+
+            //Console.WriteLine("Enter the number of tests to be averaged:");
+            //int numTest = int.Parse(Console.ReadLine());
+            //double avgScore = CalculateSum(numTest) / numTest;
+            //Console.WriteLine($"The average is {avgScore}; the letter grade is {CalculateGrade(avgScore)}");
+
+
+
+
+            //Advanced Difficulty
+            //AVERAGE A NON SPECIFIC NUMBER OF SCORES
+
+            int total = 0, count = 0;
+            string usrInput;
+            do
+            {
+                Console.WriteLine("Enter a test score or \"done\" to complete.");
+
+                usrInput = Console.ReadLine();
+                int value = 0;
+                if (int.TryParse(usrInput, out value))
+                {
+                    if (value >= 0 && value <= 100)
+                    {
+                        total += value;
+                        count++;
+                    }
+                }
+            }
+            while (usrInput != "done");
+            int average = total / count;
+            Console.WriteLine($"The average is {average}; the letter grade is {CalculateGrade(average)}");
+        }
+
+        private int CalculateSum(int numTests = 10)
+        {
+            int count = 1, tempNum, sum = 0;
+            while (count <= numTests)
+            {
+
+                Console.WriteLine($"Enter score {count}");
+                tempNum = int.Parse(Console.ReadLine());
+
+                if (tempNum >= 0 && tempNum <= 100)
+                {
+                    sum += tempNum;
+                    count++;
+                }
+                else Console.WriteLine("Please insert a number between 0 and 100");
+            }
+            return sum;
+        }
+
+        private char CalculateGrade(double gradeAverage)
+        {
+            if (gradeAverage >= 90) return 'A';
+            else if (gradeAverage >= 80) return 'B';
+            else if (gradeAverage >= 70) return 'C';
+            else if (gradeAverage >= 60) return 'D';
+            else return 'F';
+        }
+            
+    }
+}
