a0ffb453b586b7739e6e049f6ad11a1a47e89194
edavis47 <59838021+edavis47@users.noreply.github.com>
2020-01-27 13:15:52 -0500

    Add files via upload

---
+++ feetToInches/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double ft;
+            do
+            {
+                Console.WriteLine("Enter the number of feet to convert to inches:");
+            }
+            while (Double.TryParse(Console.ReadLine(), out ft) == false);
+
+            Console.WriteLine($"{ft} ft is equal to {feetToInches(ft)} inches");
+        }
+
+        static double feetToInches(double feet)
+        {
+            
+            double inch = feet * 12.00;
+            return inch;
+        }
+    }
+}
